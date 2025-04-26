0be0634553ded6c4059c35ee37b3008dcd0da431
ByronVelasco <andy-velasco10@hotmail.com>
2025-04-26 11:44:46 -0500

    Final correction

--- 1 NaiveEvaluation/NaiveEvaluation.py
+++ 1 NaiveEvaluation/NaiveEvaluation.py
@@ -3,12 +3,6 @@
 
 # %%
 def NaiveEvaluation(coeff, x):
-  """
-  Naive evaluation of a polynomial at a given point x.
-  :param coeff: list of coefficients
-  :param x: point to evaluate the polynomial at
-  :return: value of the polynomial at x
-  """
   n = len(coeff)
   result = 0
   for i in range(n):
--- 2 HornerRule/HornerRule.py
+++ 2 HornerRule/HornerRule.py
@@ -3,16 +3,6 @@
 
 # %%
 def HornerRule(coeff, x):
-  """
-  Horner's rule for evaluating a polynomial at a given point.
-  
-  Parameters:
-  coeff (list): Coefficients of the polynomial.
-  x (float): The point at which to evaluate the polynomial.
-  
-  Returns:
-  float: The value of the polynomial at x.
-  """
   result = 0
   for i in range(len(coeff)-1, -1, -1):
     result = result * x + coeff[i]
